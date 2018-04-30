a67ebd53ea6fd5e127f92dc1983c4f0d1ea410f2
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2018-04-30 12:17:33 +0200

    Quality fix

---
+++ src/main/java/pl/wavesoftware/utils/stringify/lang/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author <a href="mailto:krzysztof.suszynski@coi.gov.pl">Krzysztof Suszynski</a>
+ * @since 30.04.18
+ */
+@ParametersAreNonnullByDefault
+package pl.wavesoftware.utils.stringify.lang;
+
+import javax.annotation.ParametersAreNonnullByDefault;
